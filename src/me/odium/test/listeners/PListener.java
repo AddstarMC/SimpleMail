6e5fd74a468c9406d51f2aebc47aaf2e0480e78c
addstar <addstar@addstar.com.au>
2014-10-11 00:58:42 +1100

    Improve/cleanup player join notification

--- src/me/odium/test/listeners/PListener.java
+++ src/me/odium/test/listeners/PListener.java
@@ -15,44 +15,42 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PListener implements Listener {
 
-  public simplemail plugin;
-  public PListener(simplemail plugin) {    
-    this.plugin = plugin;    
-    plugin.getServer().getPluginManager().registerEvents(this, plugin);  
-  }
+	public simplemail plugin;
+	public PListener(simplemail plugin) {    
+		this.plugin = plugin;    
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);  
+	}
   
-  DBConnection service = DBConnection.getInstance();
+  	DBConnection service = DBConnection.getInstance();
 
-  @EventHandler(priority = EventPriority.NORMAL)
-  public void onPlayerJoin(PlayerJoinEvent event) {
-    if (plugin.getConfig().getBoolean("OnPlayerJoin.ShowNewMessages")) {
-      final Player player = event.getPlayer();
-      String targetnick = player.getName().toLowerCase();
-      Connection con;
-      java.sql.Statement stmt;
-      ResultSet rs;
-      try {        
-        con = service.getConnection();
-        stmt = con.createStatement();
-        rs = stmt.executeQuery("SELECT COUNT(target) AS inboxtotal FROM SM_Mail WHERE target='"+targetnick+"' AND isread=0");
-        if (rs.next()) {
-	      final int id = rs.getInt("inboxtotal");
-	      if(player.hasPermission("simplemail.inbox") && id != 0) {
-	        int tempDelay = plugin.getConfig().getInt("OnPlayerJoin.DelayInSeconds");
-	        int Delay = 20*tempDelay;
-	        
-	        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("SimpleMail"), new Runnable() {
-	          public void run() {
-	            player.sendMessage(plugin.GRAY+"[SimpleMail] "+plugin.GREEN+ "You have " + plugin.GOLD +id+plugin.GREEN+" new messages");
-	          }
-	        }, Delay);
-
-	      }
-        }
-        rs.close();
-      } catch(Exception e) {
-        e.printStackTrace();
-      }
-    }
-  }
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		if (plugin.getConfig().getBoolean("OnPlayerJoin.ShowNewMessages")) {
+			final Player player = event.getPlayer();
+			int Delay = plugin.getConfig().getInt("OnPlayerJoin.DelayInSeconds");
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("SimpleMail"), new Runnable() {
+				public void run() {
+					if (!player.isOnline()) return;
+					String targetnick = player.getName().toLowerCase();
+					Connection con;
+					java.sql.Statement stmt;
+					ResultSet rs;
+					try {        
+						con = service.getConnection();
+						stmt = con.createStatement();
+						rs = stmt.executeQuery("SELECT COUNT(target) AS inboxtotal FROM SM_Mail WHERE target='"+targetnick+"' AND isread=0");
+						if (rs.next()) {
+							final int total = rs.getInt("inboxtotal");
+							if(total > 0) {
+								player.sendMessage(plugin.GRAY+"[SimpleMail] "+plugin.GREEN+ "You have " + plugin.GOLD +total+plugin.GREEN+" new messages");
+							}
+						}
+						rs.close();
+					} catch(Exception e) {
+						e.printStackTrace();
+					}
+	    	    }
+			}, Delay*20L);
+		}
+	}
 }
